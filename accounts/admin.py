836b87cd73b1a715ce6d4b52d3fd16f500049c86
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-10-28 15:04:32 +0330

    write model user and forms and admin and manger

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    
+    list_display = ('email', 'mobile_phone', 'full_name', 'id')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'created_at', 'updated_at')
+    search_fields = ('email', 'full_name', 'mobile_phone', 'id')
+    filter_horizontal = ('groups', )
+    ordering = ('email', 'created_at',)
+    
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ("mobile_phone", 'full_name')}),
+        ("Permissions", {"fields": ("is_stff", 'is_active', 'is_superuser', 'groups')}),
+    ]
+    
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from core.models import CreateModel, UpdateModel
+from django.utils.translation import gettext_lazy as _
+from .manager import UsersManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin, CreateModel, UpdateModel):
+    full_name = models.CharField(_('Full name'), max_length=100)
+    email = models.EmailField(_('Email'), max_length=100, unique=True)
+    mobile_phone = models.CharField(_('Mobile phone'), max_length=11, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    
+    objects = UsersManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('full_name', 'mobile_phone')
+    
+    # def has_perm(self, perm, obj=None):
+    #     "Does the user have a specific permission?"
+    #     # Simplest possible answer: Yes, always
+    #     return True
+
+    # def has_module_perms(self, app_label):
+    #     "Does the user have permissions to view the app `app_label`?"
+    #     # Simplest possible answer: Yes, always
+    #     return True
+    
+    @property
+    def is_admin(self):
+        return self.is_staff
